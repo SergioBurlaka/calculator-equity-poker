0f1a1aec5474da81f3665ab5d4a192564bd6658d
SergioBurlaka <burlaka01@gmail.com>
2017-12-21 19:23:10 +0200

    delete old version

--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-
-
-module.exports = function (ngModule) {
-    require('./pokerTable/poker-keyboard-controller.js')(ngModule);
-};
--- directives/index.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports = function (ngModule) {
-    require('./hello-world/poker-card-major.js')(ngModule);
-    require('./card-keyboard/poker-card-small.js')(ngModule);
-};
-
